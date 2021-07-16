a8f4857c874c05d5c271c3aaaa9222be7ed9d0ac
Bradley Bossard <bradleybossard@gmail.com>
2021-07-15 19:59:49 -0700

    add mkdir function and helpers

--- src/imfs.test.ts
+++ src/imfs.test.ts
@@ -1,5 +1,7 @@
 import imfs from './imfs';
 
+const longString = 'x'.repeat(1000);
+
 describe('pwd', () => {
   test('get current working directory', () => {
     const fs = new imfs();
@@ -11,5 +13,34 @@ describe('ls', () => {
   test('returns empty listing', () => {
     const fs = new imfs();
     expect(fs.ls()).toEqual([]);
+  });
+});
+
+describe('mkdir', () => {
+  test('make a directory', () => {
+    const fs = new imfs();
+    fs.mkdir('test');
+    expect(fs.ls()).toEqual(['test']);
+  });
+
+  test('throws error when already exists', () => {
+    const fs = new imfs();
+    fs.mkdir('test');
+    expect(() => fs.mkdir('test')).toThrow(/name already exists/);
+  });
+
+  test('throws error when name is too short', () => {
+    const fs = new imfs();
+    expect(() => fs.mkdir('')).toThrow(/name too short/);
+  });
+
+  test('throws error when name is too long', () => {
+    const fs = new imfs();
+    expect(() => fs.mkdir(longString)).toThrow(/name too long/);
+  });
+
+  test('throws error when name contains invalid characters', () => {
+    const fs = new imfs();
+    expect(() => fs.mkdir('*')).toThrow(/invalid character/);
   });
 });
--- src/imfs.ts
+++ src/imfs.ts
@@ -1,3 +1,36 @@
+const MAX_NAME_LENGTH = 256;
+const INVALID_CHARACTERS = [
+  ' ',
+  '/',
+  '\\',
+  '#',
+  '@',
+  '<',
+  '>',
+  '{',
+  '}',
+  '$',
+  '?',
+  '+',
+  '`',
+  '|',
+  '=',
+  '%',
+  '*',
+  ':',
+];
+
+const ERROR_INTERNAL_PWD_CORRUPTED =
+  'Internal Error: Present Working Directory corrupted';
+const ERROR_NAME_TOO_SHORT =
+  'Proposed name too short, must be at least one character';
+const ERROR_NAME_TOO_LONG = `Proposed name too long. Must be 1-${MAX_NAME_LENGTH} characters.`;
+const ERROR_NAME_ALREADY_EXISTS = 'Proposed name already exists';
+
+const invalidCharacters = (character: string) => {
+  return `Proposed name contains invalid character: ${character}.`;
+};
+
 export default class Imfs {
   /**
    * Root of nested objects representing the filesystem.
@@ -10,6 +43,53 @@ export default class Imfs {
    * @internal
    */
   protected currentPath: string[] = [];
+
+  /**
+   * Returns an object representing the current directory.
+   * @internal
+   */
+  protected getCurrentDirectory = (): Object => {
+    let result = this.fs;
+    this.currentPath.forEach(entry => {
+      if (result !== {}) {
+        if (!(entry in result)) {
+          // This should hopefully never happen
+          throw new Error(ERROR_INTERNAL_PWD_CORRUPTED);
+        }
+        result = result[entry];
+      }
+    });
+    return result;
+  };
+
+  /**
+   * Validates a proposed file/directory name conforms to rules
+   * around allowed characters and name length.
+   * @internal
+   */
+  protected validateName = (name: string): void => {
+    if (name === '') {
+      throw new Error(ERROR_NAME_TOO_SHORT);
+    }
+    INVALID_CHARACTERS.forEach(character => {
+      if (name.includes(character)) {
+        throw new Error(invalidCharacters(character));
+      }
+    });
+    if (name.length > MAX_NAME_LENGTH) {
+      throw new Error(ERROR_NAME_TOO_LONG);
+    }
+  };
+
+  /**
+   * Validates that a new file or directory does not exist on a given node.
+   * @internal
+   */
+  protected validateCreation = (node: Object, name: string): void => {
+    if (name in node) {
+      throw new Error(ERROR_NAME_ALREADY_EXISTS);
+    }
+  };
 
   /**
    * Returns the present working directory.
@@ -45,5 +125,30 @@ export default class Imfs {
   ls = (): string[] => {
     const node = this.getCurrentDirectory();
     return Object.keys(node);
+  };
+
+  /**
+   * Create a new directory in present working directory.  Will
+   * throw an error if a directory or file does not exist, or
+   * the proposed name does not meet the naming conventions.
+   * @param name The name of the directory to be created
+   *
+   * Example:
+   * ```typescript
+   * import imfs from './imfs';
+   * const fs = new imfs();
+   * fs.mkdir('foo');
+   * console.log(fs.ls());  // '['foo']'
+   * fs.mkdir('foo');  // Throws an exception
+   * fs.mkdir('');  // Throws an exception
+   * fs.mkdir('*');  // Throws an exception
+   * fs.mkdir('x'.repeat(1000));  // Throws an exception
+   * ```
+   */
+  mkdir = (name: string): void => {
+    this.validateName(name);
+    const node = this.getCurrentDirectory();
+    this.validateCreation(node, name);
+    node[name] = {};
   };
 }
